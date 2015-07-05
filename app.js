c87e3b80b11a8e64f91b46b86d0368b4c64c88d1
David Vuong <david.vuong@uwaterloo.ca>
2015-07-05 10:41:49 -0400

    Facial recognition working

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var API_KEY = "*SKY BIOMETRY API KEY GOES HERE*";
-var API_SECRET = "*SKY BIOMETRY API SECRET GOES HERE*";
+var API_KEY = "5305005f8dd2426696adedfb6b159da1";
+var API_SECRET = "325e64602e874234af4d833e073e6f99";
 
 //initialization of webcam variables
 var width=320;
@@ -9,6 +9,9 @@ var video = null;
 var canvas = null;
 var photo = null;
 var startbutton = null;
+var recognizebutton = null;
+
+var name;
 
 function uploadFile() {
 	var formData = new FormData();
@@ -30,7 +33,25 @@ function uploadComplete(event) {
 	$.get (
 		"http://api.skybiometry.com/fc/faces/detect.json?api_key=" + API_KEY + "&api_secret=" + API_SECRET + "&urls=http://davidvuong.ca/Upload/uploads/" + event.target.responseText + "&attributes=all", function(data) {
 			console.log(data);
+			var tid = data.photos[0].tags[0].tid;
+			console.log(tid);
+			
+			name = prompt("Enter name: ");
+			console.log(name);
+			console.log("Storing tid");
+			
+			$.get("http://api.skybiometry.com/fc/tags/save.json?api_key=" + API_KEY + "&api_secret=" + API_SECRET + "&uid=" + name + "@BattleHack&tids=" + tid + "&label=" + name, saveTidCallBack);
 	});
+}
+
+function saveTidCallBack(data) {
+	console.log(data);
+	
+	$.get("http://api.skybiometry.com/fc/faces/train.json?api_key=" + API_KEY + "&api_secret=" + API_SECRET + "&uids=" + name + "@BattleHack", trainCallback);
+}
+
+function trainCallback(data) {
+	console.log(data);
 }
 
 function uploadFailed(event) {
@@ -46,6 +67,7 @@ function initializeWebcam() {
 	canvas = document.getElementById("canvas");
 	photo = document.getElementById("photo");
 	startbutton = document.getElementById("startbutton");
+	recognizebutton = document.getElementById("recognize");
 	
 	navigator.getUserMedia = ( navigator.getUserMedia ||
                            navigator.webkitGetUserMedia ||
@@ -88,6 +110,11 @@ function initializeWebcam() {
 		ev.preventDefault();
 	}, false);
 	
+	recognizebutton.addEventListener('click', function(ev) {
+		recognize();
+		ev.preventDefault();
+	}, false)
+	
 	clearPhoto();
 }
 
@@ -123,4 +150,47 @@ function takePicture() {
 	else {
       clearPhoto();
     }
+}
+
+function recognize() {
+	var context = canvas.getContext('2d');
+    if (width && height) {
+		canvas.width = width;
+		canvas.height = height;
+		context.drawImage(video, 0, 0, width, height);
+    
+		var data = canvas.toDataURL('image/png');
+		photo.setAttribute('src', data);
+		console.log(data.toString());
+		
+		var formData = new FormData();
+		formData.append("imgData", data);
+		var phpServer = new XMLHttpRequest();
+		phpServer.addEventListener("load", recognizeUploadComplete, false);
+		phpServer.addEventListener("error", recognizeUploadFailed, false);
+		phpServer.addEventListener("abort", recognizeUploadCancelled, false);
+		phpServer.open("POST", "newfile.php");
+		phpServer.send(formData);
+    } 
+	else {
+      clearPhoto();
+    }
+}
+
+function recognizeUploadComplete(event) {
+	console.log(event.target.responseText);
+	name = prompt("Enter your name: ");
+	$.get("http://api.skybiometry.com/fc/faces/recognize.json?api_key=" + API_KEY + "&api_secret=" + API_SECRET + "&urls=http://davidvuong.ca/Upload/uploads/" + event.target.responseText + "&uids=" + name + "@BattleHack", recognizeCallback);
+}
+
+function recognizeCallback(data) {
+	console.log(data);
+}
+
+function recognizeUploadFailed(event) {
+	console.log("An error occured while uploading the file.");
+}
+
+function recognizeUploadCancelled(event) {
+	console.log("The upload has been cancelled by the user or the browser dropped the connection.");
 }
